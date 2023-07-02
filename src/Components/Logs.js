6a50b80a9375d28929f9ae575fd0d03de5f04281
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-07-02 17:43:05 -0400

    logs comomponent has issues

--- src/Components/Logs.js
+++ src/Components/Logs.js
@@ -1,14 +1,41 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Log from "./Log";
+import axios from "axios";
 
-export default function NavBar() {
+const API = process.env.REACT_APP_API_URL;
+
+
+function Logs() {
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API}/bookmarks`)
+      .then((response) => setBookmarks(response.data))
+      .catch((e) => console.error("catch", e));
+  }, []);
+
   return (
-    <nav>
-      <h1>
-        <Link to="/logs">Captain's Log</Link>
-      </h1>
-      <button>
-        <Link to="/logs/new">New Log</Link>
-      </button>
-    </nav>
+    <div className="Bookmarks">
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Mistakes</th>
+              <th>Captain Name</th>
+              <th>See this log</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookmarks.map((log, index) => {
+              return <Log key={index} bookmark={log} index={index} />;
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
   );
 }
+
+export default Bookmarks;

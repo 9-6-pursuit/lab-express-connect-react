bb81a2be585ddbbaddef8bb874b4b70e6b78bffd
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-07-02 17:22:47 -0400

    more changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+// PAGES
+import Edit from "./Pages/Edit";
+import FourOFour from "./Pages/FourOfFour";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+
+// COMPONENTS
+import NavBar from "./Components/NavBar";
+
 function App() {
-  return <div>Hello World John</div>;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/bookmarks" element={<Index />} />
+            <Route path="/bookmarks/new" element={<New />} />
+            <Route path="/bookmarks/:index" element={<Show />} />
+            <Route path="/bookmarks/:index/edit" element={<Edit />} />
+            <Route path="*" element={<FourOFour />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

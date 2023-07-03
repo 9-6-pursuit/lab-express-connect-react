45f13485bea678d9b1a918dc92e6f66cb9f397cc
Lakisha J <lakishajohnson@pursuit.org>
2023-07-03 14:23:47 -0400

    Imported depencies and created Route paths to pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//PAGES
+import Edit from "./Pages/Edit";
+import Home from "./Pages/Home";
+
+
+// COMPONENTS
+// import NavBar from "./Components/NavBar";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        {/* <NavBar /> */}
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/logs/:index/edit" element={<Edit />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

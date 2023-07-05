34fcb80cc2d57db2f435f2790c861366c5bce9f7
Lakisha J <lakishajohnson@pursuit.org>
2023-07-05 11:15:12 -0400

    Show page

---
+++ src/Pages/Show.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LogDetails from "../Components/LogDetails";
+
+function Show() {
+  return (
+    <div className="Show">
+      <h2>Show</h2>
+      <section>
+        <LogDetails />
+      </section>
+    </div>
+  );
+}
+
+export default Show;

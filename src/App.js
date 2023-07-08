ab647ae8a969a2adb95f3f6bc8245d9fa1fc6d6c
Lakisha J <lakishajohnson@pursuit.org>
2023-07-08 07:15:48 -0400

    Home, Index, New, Show, Edit, and BONUS routes functioning. All tests passing.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // DEPENDENCIES
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-//PAGES
-import Edit from "./Pages/Edit";
+//PAGES AKA REACT ROUTES
 import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+import Edit from "./Pages/Edit";
 
 //COMPONENT
 import NavBar from "./NavBar";
-
+import NoLogs from "./Components/NoLogs";
 
 function App() {
   return (
@@ -17,7 +20,11 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/logs" element={<Index />} />
+            <Route path="/logs/new" element={<New />} />
+            <Route path="/logs/:index" element={<Show />} />
             <Route path="/logs/:index/edit" element={<Edit />} />
+            <Route path="*" element={<NoLogs />} />
           </Routes>
         </main>
       </Router>

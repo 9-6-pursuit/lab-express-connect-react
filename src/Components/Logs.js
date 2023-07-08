a803177031375c9744759014e7cf9b5f7536b458
Lakisha J <lakishajohnson@pursuit.org>
2023-07-08 07:01:02 -0400

    Added bootstrap CSS to table

--- src/Components/Logs.js
+++ src/Components/Logs.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import CaptainsLog from "./Captain's Log";
+import CaptainsLog from "./CaptainsLog";
+import "../logs.css";
 
 function Logs() {
   const [logs, setLogs] = useState([]);
@@ -11,27 +12,32 @@ function Logs() {
       // console.log("we hit useEffect!");
       const response = await axios.get(`${URL}/logs`);
       setLogs(response.data);
-    //   console.log(response.data);
+      //   console.log(response.data);
     };
     result();
-  }, []);
+  });
+
   return (
-    <div className="CaptainsLog">
-      <section>
-        <table>
-          <thead>
-            <tr>
-              <th></th>
-              <th>See this Log</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {logs.map((log, i) => {
-              return <CaptainsLog key={i} log={log} index={i} />;
-            })}
-          </tbody>
-        </table>
+    <div className="Log">
+      <section className="d-flex justify-content-center align-items-center vh-10">
+        <div className="card w-75">
+          <div className="card-body">
+            <table className="custom-table">
+              <thead>
+                <tr>
+                  <th scope="col">Mistakes</th>
+                  <th scope="col">Captain Name</th>
+                  <th scope="col">See this log</th>
+                </tr>
+              </thead>
+              <tbody>
+                {logs.map((log, i) => {
+                  return <CaptainsLog key={i} log={log} index={i} />;
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </section>
     </div>
   );

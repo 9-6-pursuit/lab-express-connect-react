a2dd31e1319d25b4bc2d393b59361752ea5358c2
Raymond <raymondjohnson@pursuit.org>
2023-07-09 08:51:33 -0400

    In progress...only NavBar complete

---
+++ src/Components/NavBar.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <h1>
+        <Link to="/">Captain's Log</Link>
+      </h1>
+      <button>
+        <Link to="/">Home</Link>
+      </button>
+      <button>
+        <Link to="/logs">Logs</Link>
+      </button>
+      <button>
+        <Link to="/logs/new">New Logs</Link>
+      </button>
+    </nav>
+  );
+}

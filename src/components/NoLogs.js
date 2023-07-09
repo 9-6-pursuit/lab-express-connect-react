e2102bedc29980507b56939c2d22211f098a0693
Monir Hossen <monirhossen@pursuit.org>
2023-07-09 12:15:51 -0400

    Components created

---
+++ src/components/NoLogs.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NoLogs() {
+  return (
+    <div className="noLogs-container">
+      <img
+        src="https://i.imgflip.com/3ts0nb.jpg"
+        alt="No videos"
+        className="meme"
+      />
+    </div>
+  );
+}
+
+export default NoLogs;

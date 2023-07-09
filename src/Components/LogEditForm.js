a7c1360388ae8449784d60e180f4c461c37c9f02
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-07-09 06:29:17 -0400

    Not done, but I have to submit it

---
+++ src/Components/LogEditForm.js
@@ -0,0 +1,106 @@
+import { useState, useEffect } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+const API = process.env.REACT_APP_API_URL;
+
+function LogEditForm() {
+  let { index } = useParams();
+  let navigate = useNavigate()
+
+  const [log, setLog] = useState({
+    name: "",
+    url: "",
+    category: "",
+    description: "",
+    isFavorite: false,
+  });
+
+  const updateLog = () => {
+    axios
+      .put(`${API}/logs/${index}`, log)
+      .then((response) => {
+        setLog(response.data);
+        navigate(`/logs/${index}`);
+      })
+      .catch((c) => console.warn("catch", c));
+  };
+
+  const handleTextChange = (event) => {
+    setLog({ ...log, [event.target.id]: event.target.value });
+  };
+
+  const handleCheckboxChange = () => {
+    setLog({ ...log, isFavorite: !log.isFavorite });
+  };
+
+  useEffect(() => {
+    axios
+      .get(`${API}/logs/${index}`)
+      .then((response) => {
+        setLog(response.data);
+      })
+      .catch((e) => console.error(e));
+  }, [index]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateLog()
+  };
+  return (
+    <div className="Edit">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          value={log.name}
+          type="text"
+          onChange={handleTextChange}
+          placeholder="Name of Website"
+          required
+        />
+        <label htmlFor="url">URL:</label>
+        <input
+          id="url"
+          type="text"
+          pattern="http[s]*://.+"
+          required
+          value={log.url}
+          placeholder="http://"
+          onChange={handleTextChange}
+        />
+        <label htmlFor="category">Category:</label>
+        <input
+          id="category"
+          type="text"
+          name="category"
+          value={log.category}
+          placeholder="educational, inspirational, ..."
+          onChange={handleTextChange}
+        />
+        <label htmlFor="isFavorite">Favorite:</label>
+        <input
+          id="isFavorite"
+          type="checkbox"
+          onChange={handleCheckboxChange}
+          checked={log.isFavorite}
+        />
+        <label htmlFor="description">Description:</label>
+        <textarea
+          id="description"
+          name="description"
+          value={log.description}
+          onChange={handleTextChange}
+          placeholder="Describe why you loged this site"
+        />
+        <br />
+
+        <input type="submit" />
+      </form>
+      <Link to={`/logs/${index}`}>
+        <button>Nevermind!</button>
+      </Link>
+    </div>
+  );
+}
+
+export default LogEditForm;
---
+++ src/Components/Logs.js
@@ -0,0 +1,44 @@
+// import { useState } from "react";
+import { useState, useEffect } from "react";
+import Log from "./Log";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL;
+console.log("here's the API url:")
+console.log(API)
+
+function Logs() {
+  // const [logs] = useState([]);
+  const [logs, setLogs] = useState([]);
+  
+  useEffect(() => {
+    axios
+      .get(`${API}/logs`)
+      .then((response) => setLogs(response.data))
+      .catch((e) => console.error("catch", e));
+  }, []);
+
+  return (
+    <div className="Logs">
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Mistakes</th>
+              <th>Captain's Name</th>
+              <th>See this log</th>
+            </tr>
+          </thead>
+          <tbody>
+            {logs.map((log, index) => {
+              return <Log key={index} log={log} index={index} />;
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+}
+
+export default Logs;

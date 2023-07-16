70b5601df7d76e1cd3399500adbed6a2e5785fce
KleemoffDeveloper <siliskleemoff@pursuit.org>
2023-07-16 00:39:09 -0400

    here is the lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+import Edit from "./Pages/Edit";
+import ErrorFour from "./Pages/ErrorFour";
+import NavBar from "./Components/NavBar";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/logs" element={<Index />} />
+            <Route path="/logs/new" element={<New />} />
+            <Route path="/logs/:index" element={<Show />} />
+            <Route path="/logs/:index/edit" element={<Edit />} />
+            <Route path="*" element={<ErrorFour />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

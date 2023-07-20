3a2f7aec20c91b61966414f1d23f7004eb2d089c
atiradoGit88 <angeltirado@pursuit.org>
2023-07-20 01:42:00 -0400

    final commit.

---
+++ src/Pages/Edit.js
@@ -0,0 +1,10 @@
+import LogEditForm from "../components/LogEditForm";
+
+export default function Edit() {
+  return (
+    <div className="New Edit">
+      <h2>Edit</h2>
+      <LogEditForm />
+    </div>
+  );
+};
